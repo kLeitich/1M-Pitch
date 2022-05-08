697ee2ebac1fb1522ebc7d2474844bc4943c5a70
kleitich <kevleitich@gmail.com>
2022-05-09 02:27:02 +0300

    adding init with code

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,8 @@ from config import config_options
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-
-    app = Flask(__name__)
-
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
-
+    #....
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -22,4 +15,5 @@ def create_app(config_name):
     configure_request(app)
 
     return app
+
 
