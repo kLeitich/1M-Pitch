4be7ba2af4c61de96fdb8baae201543dd34f1bad
kleitich <kevleitich@gmail.com>
2022-05-09 10:40:47 +0300

    adding files to the repo and trying to debug

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,10 +2,9 @@
 from flask import render_template,redirect,url_for,flash,request
 from ..models import User
 from .forms import LoginForm,RegistrationForm
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from .. import db
 from . import auth
-
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -15,7 +14,10 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+       
+
         return redirect(url_for('auth.login'))
+        
     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/login',methods=['GET','POST'])
@@ -25,9 +27,14 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(url_for('main.index'))
 
         flash('Invalid username or Password')
 
-    
     return render_template('auth/login.html',login_form = login_form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
